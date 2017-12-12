fd5abafc7d7238dba458158faa90473b6c90daa7
Sara Rykov <sara.rykov@gmail.com>
2017-12-11 19:18:45 -0500

    Add cells & their event listener to change cell color

--- designs.js
+++ designs.js
@@ -3,25 +3,35 @@ $('#sizePicker').submit(function(e){
 	makeGrid();
 });
 
-function makeGrid(){
-	// Select size input
-	let height = $('#input_height').val();
-	let width = $('#input_width').val();
 
+
+function makeGrid(){
+	//get table element, remove existing rows & cells
 	let table = $('#pixel_canvas');
 	table.empty();
 
-	//create  rows 
+	// get size input from form
+	let height = $('#input_height').val();
+	let width = $('#input_width').val();
+
+	//create rows 
 	for(let i = 0; i < height; i++){
 		let tableRow = $('<tr></tr>');
 		table.append(tableRow);
 
-		//create cells
+		//create cells for each row
 		for(let j = 0; j < width; j++){
 			let cell = $('<td></td>');
+
+			//add event listener to each cell
+			cell.click(function(){
+				//change background
+				$(this).attr('bgcolor', $('#colorPicker').val())
+			});
+
+			//add cell to table row
 			tableRow.append(cell);
 		}
 
 	}
-
 }
